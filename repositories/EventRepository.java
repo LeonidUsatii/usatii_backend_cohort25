b2817ad476f69c96b8a1393b7a5303b0a83526aa
Leonid Usatii <leonid.usatiy@gmail.com>
2023-09-09 18:08:08 +0200

    delete folders

--- repositories/EventRepository.java
+++
@@ -1,8 +0,0 @@
-package de.ait.task_02.repositories;
-import de.ait.task_02.models.Event;
-
-public interface EventRepository extends CrudRepository<Event>{
-
-    Event findOneByTitle(String title);
-
-}
